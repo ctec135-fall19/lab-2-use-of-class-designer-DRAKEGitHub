c9ee504c0c4debe238a0a1bdfffcccccfb6d512a
Allison Drake <allisonnicoledrake@gmail.com>
2019-10-01 15:28:13 -0700

    partially complete lab 2

---
+++ MyLabTwo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLabTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up Console UI (CUI)
+            Console.Title = "This is confusing to a new kid";
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Without Visual References or Instructions, It's confusing to follow these assignments");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            // Wait for Enter key to be pressed.
+            Console.ReadLine();
+        }
+    }
+}
